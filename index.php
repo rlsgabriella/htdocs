4f96e55175015a23a887a39f1279c73e3a039257
Gabriella Rodrigues <rlsgabriella@gmail.com>
2024-07-08 21:46:55 -0300

    inicei CRUD

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+
+
+    <p>Código PHP</p>
+
+    <?php
+    $servername = "localhost"; // local
+    $username = "root";
+    $password = "Gabirock.95";
+    $dbname = "teste";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // $sql = "SELECT id, firstname, lastname FROM MyGuests";
+    $sql = "SELECT * FROM teste.alunos";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
